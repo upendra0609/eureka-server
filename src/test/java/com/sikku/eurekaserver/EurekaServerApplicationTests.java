2d78b81636d75a5b5788933dc26493aeb3cd3c2c
upendra <upendrasatna@gmail.com>
2024-04-12 00:39:05 +0530

    initial

---
+++ src/test/java/com/sikku/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sikku.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
